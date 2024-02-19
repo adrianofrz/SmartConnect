0f444d5ed52861e21feacdc2b5353c3316d4fb3f
Adriano Ferraz <ferraz.adriano3@gmail.com>
2024-02-18 22:02:03 -0300

    Scan de redes

--- smartconnect/lib/main.dart
+++ smartconnect/lib/main.dart
@@ -1,168 +1,235 @@
-import 'dart:io';
+import 'dart:async';
 
-import 'package:baseflow_plugin_template/baseflow_plugin_template.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:smartconnect/permission_app.dart';
+import 'package:smartconnect/wifi_manager.dart';
+import 'package:wifi_scan/wifi_scan.dart';
 
 void main() {
-  //PermissionApp requestPermission = PermissionApp();
-  //requestPermission.requestPermissionWifi();
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(BaseflowPluginExample(
-      pluginName: 'Permission Handler',
-      githubURL: 'https://github.com/Baseflow/flutter-permission-handler',
-      pubDevURL: 'https://pub.dev/packages/permission_handler',
-      pages: [PermissionHandlerWidget.createPage()]));
+  runApp(const MyApp());
 }
 
-///Defines the main theme color
-final MaterialColor themeMaterialColor =
-    BaseflowPluginExample.createMaterialColor(
-        const Color.fromRGBO(48, 49, 60, 1));
-
-/// A Flutter application demonstrating the functionality of this plugin
-class PermissionHandlerWidget extends StatefulWidget {
-  /// Create a page containing the functionality of this plugin
-  static ExamplePage createPage() {
-    PermissionApp requestPermission = PermissionApp();
-    requestPermission.requestPermissionWifi();
-    return ExamplePage(
-        Icons.location_on, (context) => PermissionHandlerWidget());
-  }
-
+/// Example app for wifi_scan plugin.
+class MyApp extends StatefulWidget {
+  /// Default constructor for [MyApp] widget.
+  const MyApp({Key? key}) : super(key: key);
   @override
-  _PermissionHandlerWidgetState createState() =>
-      _PermissionHandlerWidgetState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _PermissionHandlerWidgetState extends State<PermissionHandlerWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-          children: Permission.values
-              .where((permission) {
-                if (Platform.isIOS) {
-                  return permission != Permission.unknown &&
-                      permission != Permission.phone &&
-                      permission != Permission.sms &&
-                      permission != Permission.ignoreBatteryOptimizations &&
-                      permission != Permission.accessMediaLocation &&
-                      permission != Permission.activityRecognition &&
-                      permission != Permission.manageExternalStorage &&
-                      permission != Permission.systemAlertWindow &&
-                      permission != Permission.requestInstallPackages &&
-                      permission != Permission.accessNotificationPolicy &&
-                      permission != Permission.bluetoothScan &&
-                      permission != Permission.bluetoothAdvertise &&
-                      permission != Permission.bluetoothConnect &&
-                      permission != Permission.nearbyWifiDevices &&
-                      permission != Permission.videos &&
-                      permission != Permission.audio &&
-                      permission != Permission.scheduleExactAlarm &&
-                      permission != Permission.sensorsAlways;
-                } else {
-                  return permission != Permission.unknown &&
-                      permission != Permission.mediaLibrary &&
-                      permission != Permission.photosAddOnly &&
-                      permission != Permission.reminders &&
-                      permission != Permission.bluetooth &&
-                      permission != Permission.appTrackingTransparency &&
-                      permission != Permission.criticalAlerts &&
-                      permission != Permission.assistant;
-                }
-              })
-              .map((permission) => PermissionWidget(permission))
-              .toList()),
-    );
-  }
-}
+class _MyAppState extends State<MyApp> {
+  List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
+  StreamSubscription<List<WiFiAccessPoint>>? subscription;
+  bool shouldCheckCan = true;
 
-/// Permission widget containing information about the passed [Permission]
-class PermissionWidget extends StatefulWidget {
-  /// Constructs a [PermissionWidget] for the supplied [Permission]
-  const PermissionWidget(this._permission);
+  bool get isStreaming => subscription != null;
 
-  final Permission _permission;
+  Future<void> _startScan(BuildContext context) async {
+    // check if "can" startScan
+    if (shouldCheckCan) {
+      // check if can-startScan
+      final can = await WiFiScan.instance.canStartScan();
+      // if can-not, then show error
+      if (can != CanStartScan.yes) {
+        if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
+        return;
+      }
+    }
 
-  @override
-  _PermissionState createState() => _PermissionState(_permission);
-}
-
-class _PermissionState extends State<PermissionWidget> {
-  _PermissionState(this._permission);
-
-  final Permission _permission;
-  PermissionStatus _permissionStatus = PermissionStatus.denied;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _listenForPermissionStatus();
+    // call startScan API
+    final result = await WiFiScan.instance.startScan();
+    if (mounted) kShowSnackBar(context, "startScan: $result");
+    // reset access points.
+    setState(() => accessPoints = <WiFiAccessPoint>[]);
   }
 
-  void _listenForPermissionStatus() async {
-    final status = await _permission.status;
-    setState(() => _permissionStatus = status);
+  Future<bool> _canGetScannedResults(BuildContext context) async {
+    if (shouldCheckCan) {
+      // check if can-getScannedResults
+      final can = await WiFiScan.instance.canGetScannedResults();
+      // if can-not, then show error
+      if (can != CanGetScannedResults.yes) {
+        if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
+        accessPoints = <WiFiAccessPoint>[];
+        return false;
+      }
+    }
+    return true;
   }
 
-  Color getPermissionColor() {
-    switch (_permissionStatus) {
-      case PermissionStatus.denied:
-        return Colors.red;
-      case PermissionStatus.granted:
-        return Colors.green;
-      case PermissionStatus.limited:
-        return Colors.orange;
-      default:
-        return Colors.grey;
+  Future<void> _getScannedResults(BuildContext context) async {
+    WifiManager wifi = WifiManager(context);
+    final results = await wifi.getScannedResults();
+    setState(() => accessPoints = results!);
+
+    /*if (await _canGetScannedResults(context)) {
+      // get scanned results
+      final results = await WiFiScan.instance.getScannedResults();
+      setState(() => accessPoints = results);
+    }*/
+  }
+
+  Future<void> _startListeningToScanResults(BuildContext context) async {
+    if (await _canGetScannedResults(context)) {
+      subscription = WiFiScan.instance.onScannedResultsAvailable
+          .listen((result) => setState(() => accessPoints = result));
     }
   }
 
+  void _stopListeningToScanResults() {
+    subscription?.cancel();
+    setState(() => subscription = null);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // stop subscription for scanned results
+    _stopListeningToScanResults();
+  }
+
+  // build toggle with label
+  Widget _buildToggle({
+    String? label,
+    bool value = false,
+    ValueChanged<bool>? onChanged,
+    Color? activeColor,
+  }) =>
+      Row(
+        children: [
+          if (label != null) Text(label),
+          Switch(value: value, onChanged: onChanged, activeColor: activeColor),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        _permission.toString(),
-        style: Theme.of(context).textTheme.bodyLarge,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+          actions: [
+            _buildToggle(
+                label: "Check can?",
+                value: shouldCheckCan,
+                onChanged: (v) => setState(() => shouldCheckCan = v),
+                activeColor: Colors.purple)
+          ],
+        ),
+        body: Builder(
+          builder: (context) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.refresh),
+                      label: const Text('GET'),
+                      onPressed: () async => _getScannedResults(context),
+                    ),
+                    _buildToggle(
+                      label: "STREAM",
+                      value: isStreaming,
+                      onChanged: (shouldStream) async => shouldStream
+                          ? await _startListeningToScanResults(context)
+                          : _stopListeningToScanResults(),
+                    ),
+                  ],
+                ),
+                const Divider(),
+                Flexible(
+                  child: Center(
+                    child: accessPoints.isEmpty
+                        ? const Text("NO SCANNED RESULTS")
+                        : ListView.builder(
+                            itemCount: accessPoints.length,
+                            itemBuilder: (context, i) =>
+                                _AccessPointTile(accessPoint: accessPoints[i])),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      subtitle: Text(
-        _permissionStatus.toString(),
-        style: TextStyle(color: getPermissionColor()),
-      ),
-      trailing: (_permission is PermissionWithService)
-          ? IconButton(
-              icon: const Icon(
-                Icons.info,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                checkServiceStatus(
-                    context, _permission as PermissionWithService);
-              })
-          : null,
-      onTap: () {
-        requestPermission(_permission);
-      },
     );
   }
+}
 
-  void checkServiceStatus(
-      BuildContext context, PermissionWithService permission) async {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text((await permission.serviceStatus).toString()),
-    ));
+/// Show tile for AccessPoint.
+///
+/// Can see details when tapped.
+class _AccessPointTile extends StatelessWidget {
+  final WiFiAccessPoint accessPoint;
+
+  const _AccessPointTile({Key? key, required this.accessPoint})
+      : super(key: key);
+
+  // build row that can display info, based on label: value pair.
+  Widget _buildInfo(String label, dynamic value) => Container(
+        decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey)),
+        ),
+        child: Row(
+          children: [
+            Text(
+              "$label: ",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Expanded(child: Text(value.toString()))
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    final title = accessPoint.ssid.isNotEmpty ? accessPoint.ssid : "**EMPTY**";
+    final signalIcon = accessPoint.level >= -80
+        ? Icons.signal_wifi_4_bar
+        : Icons.signal_wifi_0_bar;
+    return ListTile(
+      visualDensity: VisualDensity.compact,
+      leading: Icon(signalIcon),
+      title: Text(title),
+      subtitle: Text(accessPoint.capabilities),
+      onTap: () => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(title),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildInfo("BSSDI", accessPoint.bssid),
+              _buildInfo("Capability", accessPoint.capabilities),
+              _buildInfo("frequency", "${accessPoint.frequency}MHz"),
+              _buildInfo("level", accessPoint.level),
+              _buildInfo("standard", accessPoint.standard),
+              _buildInfo(
+                  "centerFrequency0", "${accessPoint.centerFrequency0}MHz"),
+              _buildInfo(
+                  "centerFrequency1", "${accessPoint.centerFrequency1}MHz"),
+              _buildInfo("channelWidth", accessPoint.channelWidth),
+              _buildInfo("isPasspoint", accessPoint.isPasspoint),
+              _buildInfo(
+                  "operatorFriendlyName", accessPoint.operatorFriendlyName),
+              _buildInfo("venueName", accessPoint.venueName),
+              _buildInfo("is80211mcResponder", accessPoint.is80211mcResponder),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
 
-  Future<void> requestPermission(Permission permission) async {
-    final status = await permission.request();
-
-    setState(() {
-      print(status);
-      _permissionStatus = status;
-      print(_permissionStatus);
-    });
-  }
+/// Show snackbar.
+void kShowSnackBar(BuildContext context, String message) {
+  if (kDebugMode) print(message);
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(SnackBar(content: Text(message)));
 }
--- smartconnect/lib/wifi_manager.dart
+++ smartconnect/lib/wifi_manager.dart
@@ -1,2 +1,45 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'package:flutter/services.dart';
+import 'package:wifi_scan/wifi_scan.dart';
+import 'package:flutter/foundation.dart';
+
+class WifiManager {
+  List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
+  StreamSubscription<List<WiFiAccessPoint>>? subscription;
+  bool shouldCheckCan = true;
+  // ignore: recursive_getters
+  BuildContext get context => context;
+
+  WifiManager(BuildContext context);
+  bool get isStreaming => subscription != null;
+
+  Future<bool> _canGetScannedResults() async {
+    if (shouldCheckCan) {
+      // check if can-getScannedResults
+      final can = await WiFiScan.instance.canGetScannedResults();
+      // if can-not, then show error
+      if (can != CanGetScannedResults.yes) {
+        //if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
+        accessPoints = <WiFiAccessPoint>[];
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // ignore: unused_element
+  Future<List<WiFiAccessPoint>?> getScannedResults() async {
+    if (await _canGetScannedResults()) {
+      // get scanned results
+      final results = await WiFiScan.instance.getScannedResults();
+      return results;
+      // ignore: use_build_context_synchronously
+      //context.setState(() => accessPoints = results);
+    }
+    return null;
+  }
+
+  void checkWifi() async {}
+}
