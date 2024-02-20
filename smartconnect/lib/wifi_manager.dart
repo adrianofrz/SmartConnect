a7f9d44af9ffc43c4eb9a756454c4f5529be48db
Adriano Ferraz <ferraz.adriano3@gmail.com>
2024-02-19 22:50:43 -0300

    nova tela informativa

--- smartconnect/lib/wifi_manager.dart
+++ smartconnect/lib/wifi_manager.dart
@@ -41,5 +41,38 @@ class WifiManager {
     return null;
   }
 
-  void checkWifi() async {}
+  void checkWifi() async {
+    // Verifica se o Wi-Fi está ativo
+    bool isWifiEnabled = await WiFiForIoTPlugin.isEnabled();
+    if (!isWifiEnabled) {
+      // Wi-Fi não está ativo. Retorne ou exiba uma mensagem para o usuário.
+      return;
+    }
+
+    //// Obtém informações da rede Wi-Fi conectada
+    String? connectionInfo = await WiFiForIoTPlugin.getWiFiAPPreSharedKey();
+//
+    //// Verifica se a rede conectada é de 2.4 GHz
+    //bool is24ghz = connectionInfo.frequency == 2400;
+    //if (!is24ghz) {
+    //  // Dispositivo conectado a uma rede de 5 GHz. Retorne ou exiba uma mensagem para o usuário.
+    //  return;
+    //}
+//
+    //// Lista as redes Wi-Fi disponíveis
+    //List<WifiScanResult> scanResults = await WiFiScan.getWifiList();
+//
+    //// Filtra as redes de 5 GHz com rssi maior ou igual a -70
+    //List<WifiScanResult> ghz5Networks = scanResults
+    //    .where((result) => result.frequency == 5000 && result.level >= -70)
+    //    .toList();
+//
+    //// Se encontrar uma rede de 5 GHz, conecta-se a ela
+    //if (ghz5Networks.isNotEmpty) {
+    //  WifiScanResult targetNetwork = ghz5Networks.first;
+    //  await WifiIot.connect(targetNetwork.ssid, targetNetwork.password);
+    //} else {
+    //  // Nenhuma rede de 5 GHz disponível com rssi >= -70. Retorne ou exiba uma mensagem para o usuário.
+    //}
+  }
 }
